62b0d9fac020483e76113c1823549f2a667ce52b
Bastian Inuk Christensen <inuk@ruc.dk>
2020-09-23 08:59:05 +0000

    Fix indent in multiline string in test

--- Tests/SwiftPHPTests/SwiftPHPTests.swift
+++ Tests/SwiftPHPTests/SwiftPHPTests.swift
@@ -4,18 +4,18 @@ import XCTest
 final class SwiftPHPTests: XCTestCase {
     func testClass() {
         let phpClass = """
-                       <?php
-                       class SimpleClass
-                       {
-                           // property declaration
-                           public $test = 'This works';
+<?php
+class SimpleClass
+{
+    // property declaration
+    public $test = 'This works';
 
-                           // method declaration
-                           public function displayVar() {
-                               echo $this->test;
-                           }
-                       }
-                       ?>
+    // method declaration
+    public function displayVar() {
+        echo $this->test;
+    }
+}
+?>
                        """
         let phpObject = PHPObject(class: phpClass)
         XCTAssertEqual(phpObject.test, "This works")
